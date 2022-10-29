47d2f5aa05f04aef68d08bbe51d38718e60c17af
Mun Duk Hyun <dev.moonduck@gmail.com>
2022-10-29 23:33:43 +0700

    wip

--- dags/jobs/covid_pipeline.py
+++ dags/jobs/covid_pipeline.py
@@ -1,8 +1,7 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python_operator import PythonVirtualenvOperator
 from datetime import datetime, timedelta
-from tasks import covid_exporter
 
 COVID_ENDPOINT_TMPL = "https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data" \
                  + f"/csse_covid_19_daily_reports/%s.csv"
@@ -12,15 +11,20 @@ OUTPUT_PATH = "/shared_dir/covid.csv"
 with DAG('covid',
          start_date=datetime(2022, 10, 1),
          max_active_runs=3,
-         schedule_interval="*/10 * * * *") as dag:
+         schedule_interval="0 * * * *") as dag:
     date_to_download = (datetime.today() - timedelta(days=30)).strftime("%m-%d-%Y")
 
     endpoint = COVID_ENDPOINT_TMPL % date_to_download
     downloader = BashOperator(
         task_id='download',
-        bash_command=f'curl -i -k -o {OUTPUT_PATH} {endpoint}')
+        bash_command=f'curl -k -o {OUTPUT_PATH} {endpoint}')
 
-    printer = PythonOperator(
+    from tasks import covid_exporter
+
+    exporter = PythonVirtualenvOperator(
         task_id='print',
-        python_callable=covid_exporter.run
+        python_callable=covid_exporter.run,
+        requirements=["psycopg==3.1.4"]
     )
+
+    downloader >> exporter
--- dags/tasks/covid_exporter.py
+++ dags/tasks/covid_exporter.py
@@ -1,15 +1,24 @@
 import csv
 
 
+def export_to_postgres(data):
+    import psycopg
+
+    with psycopg.connect("host=postgres.server.local db=dataset "
+                         + "username=dataengineer password=dataengineer") as conn:
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO covid values(%s, %s, %s, %s)", data)
+        conn.commit()
+
+
 def run():
-    header = None
     data = []
     with open("/shared_dir/covid.csv", "r") as f:
         csv_data = csv.reader(f, delimiter=',')
         got_header = False
         for row in csv_data:
             if not got_header:
-                header = row
                 got_header = True
                 continue
-            print(row[3])
+            data.append(row[3], row[7], row[8], row[4][0:10])
+    export_to_postgres(data)
