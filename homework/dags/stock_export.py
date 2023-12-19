2046ac1f2c77e660539707b586d582b410f2ee69
박은지 <sanji77@g.skku.edu>
2023-12-20 01:19:37 +0900

    과제 제출
    과제 제출합니다!

---
+++ homework/dags/stock_export.py
@@ -0,0 +1,51 @@
+def run(datadate):
+    import csv
+    import xmltodict
+    import json
+    import psycopg
+
+    # postgres로 export하는 함수 설정
+    def export_to_postgres(data):
+        with psycopg.connect(f"host=postgres-hw dbname=stock "
+                             + f"user=stock-importer password=stock") as conn:
+            with conn.cursor() as cur:
+                cur.executemany(
+                    """
+                    INSERT INTO stock
+                    VALUES (%s, %s, %s, %s, %s)
+                    """, data)
+            conn.commit()
+
+    # code
+    with open(f"/shared_dir/code.csv", "r") as f:
+        code_csv = csv.reader(f, delimiter=',')
+        header = False
+        code_data = []
+        for row in code_csv:
+            if not header:
+                header = True
+                continue
+            code_data.append(row)
+    codes = dict(code_data)
+
+    # category
+    with open(f"/shared_dir/category.xml", "r") as f:
+        category_xml = xmltodict.parse(f.read())['categories']['category']
+    cate = {row['code']: row['name'] for row in category_xml}
+
+    with open(f"/shared_dir/price_{datadate}.json", "r") as f:
+        price_json = json.load(f)
+
+    price_data = []
+
+    # 공통 key인 'code'로 코드, 카테고리 테이블에 있는 주식명, 주식 카테고리 value도 함께 가져옴
+    for row in price_json:
+        price_data.append(
+            (row['code'],
+             codes[row['code']],
+             int(row['price']),
+             datadate,
+             cate[row['code']])
+        )
+
+    export_to_postgres(price_data)
